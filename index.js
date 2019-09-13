55e317eddea52c87867f910923279731db22402d
MarkRiaz <markriaz@mail.ru>
2019-09-13 15:10:29 +0300

    project

---
+++ index.js
@@ -0,0 +1,34 @@
+let myAge;
+let myName;
+
+myName = "Michael";
+
+myName = 2;
+
+myName = undefined;
+
+myName = "Vlad";
+
+myName = myName = 2;
+
+console.log(typeof myName) // string
+console.log(typeof myAge) // number
+
+var array = [1,false,"232323", [1,2,3,4,5] ,5,6,7,8,9];
+array[4567] // undefined
+
+var user = {
+    name: "Michael",
+    surname: "Kronenberg",
+    friends: ["Amber", "Ann", {
+        name: "test",
+        surname: "test 5"
+    }]
+};
+
+var y = 2;
+var y = 4;
+var y = 10;
+var y = 50;
+
+console.log(y)
