98728c657944f1d642e94ec7f7d8f6f135653b33
Radique1 <55276119+Radique1@users.noreply.github.com>
2019-09-20 17:51:01 +0300

    Completed homework

--- index.js
+++ index.js
@@ -219,12 +219,3 @@ function randomColor() {
     input.value = "#" + randomResult;
     inputChange();
 }
-
-
-
-
-//var q = "Testing google search";
-//document.getElementById('search').onclick = function() {
-//    window.open('http://google.com/search?q='+q);
-//};
-
